ed8c8a6ea2d099f02ecb7a99b4f6b05d68968491
Fergal Condron <condrof@gmail.com>
2012-07-26 15:45:42 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscoveryProgramme::Application.config.secret_token = 'eae477181f6e43c12b0d96ac1648f7dfdf15b02bca4e5cf7e77988f58de2ac76fcc152d4be35e743cf34e136f2070729227abe916982525b9ce1e818ea57206d'
