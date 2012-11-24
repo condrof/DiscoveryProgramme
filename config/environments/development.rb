a6deb83a668bf83976a18778a228b365c7e8d839
fergal condron <condrof@gmail.com>
2012-11-24 20:57:57 +0000

    improve performance maybe

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,5 +47,5 @@ DiscoveryProgramme::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
